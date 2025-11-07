1f91bd0b195916d3ba0667eec69ba42b1c10013d
jareddelgado454 <jareddelgadotejada.41@gmail.com>
2025-11-07 11:27:24 -0500

    last changes wdd131 w1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Mostrar el año actual
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Mostrar la fecha de última modificación
+document.getElementById("lastModified").textContent = "Última actualización: " + document.lastModified;
